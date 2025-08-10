99fd51345b93dc399973a72b524270d148b18101
MD PARVEJ <mparvejind@gmail.com>
2025-08-10 11:15:03 +0530

    add variables

---
+++ 03_VARIABLES/01_basics.py
@@ -0,0 +1,19 @@
+# In  Python, variables are used to store data that can be used and manipulated throughout a program. A variable is created the moment you assign a value to it using the assighment operator (=).
+
+age = 34 # integer
+name = "Raju" # string 
+cgpa = 4.55 # float
+
+# Rule of defining a variable in Python
+# Variable names mujst start with a letter (a-z, A-Z) or an understore (_).
+# They can contain letters, numbers, and underscores.
+# Variable names are case-sensitive (age and Age are different).
+# Avoid using Python keywords (e.g., if, for, while) as variable names.
+
+34age = 4 # Invalid because variable cannot star with a number
+age = 32 # Valid because variable can start with a number
+a$$ge = 45 # Invalid because variables cannot contain special characters other than _
+_age = 34
+_nice_45 = 34
+a_b_c_7 = 34
+a_b_c_7 = "Sam"
