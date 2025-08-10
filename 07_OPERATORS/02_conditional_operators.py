f3f23dc3a350ae49a11e46c8dba62762697dd5d6
MD PARVEJ <mparvejind@gmail.com>
2025-08-10 18:19:58 +0530

    .

---
+++ 07_OPERATORS/02_conditional_operators.py
@@ -0,0 +1,12 @@
+a = 34
+
+b = 32
+
+# Conditional Operators
+print(a>4)
+print(a<4)
+print(a<=4)
+print(a>=4)
+print(a==4) # Is a equal to 4?
+print(a==34) # Is a equal to 34?
+print(a!=34) # Is a no equal to 34?
