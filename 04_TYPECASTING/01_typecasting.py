a16a9ca6ec75a7f2d5532d2ff27200782350fca9
MD PARVEJ <mparvejind@gmail.com>
2025-08-10 11:29:57 +0530

    .

---
+++ 04_TYPECASTING/01_typecasting.py
@@ -0,0 +1,19 @@
+a = 34
+b = "34"
+d = 223
+
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+# Converter b to an integer
+c = int(b)
+print(c)
+print(type(c))
+
+3 = str(d)
+print(e)
+print(type(e))
