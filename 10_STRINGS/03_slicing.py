799bb391ae67ab54d63ba68ff07d1f0171cc628f
MD PARVEJ <mparvejind@gmail.com>
2025-08-17 08:17:23 +0530

    .

---
+++ 10_STRINGS/03_slicing.py
@@ -0,0 +1,13 @@
+name = "Raju0123456789"
+
+# print(name[0:2]) # goes to from 0 to 2-1 i.e 0 to 1
+
+# print(name[2:-1]) # same as name[2:4]
+
+# print(name[0:10:n]) # skip n-1 characters
+print(name[0:10:1]) # skip 0 characters
+print(name[0:10:2]) # skip 1 characters
+print(name[0:10:3]) # skip 3-1 i.e. 2 characters
+
+print(name[:4]) # Replace the first empty number with 0 # name[0:4]
+print(name[1:]) # Replace the second empty number with length # name[1:15]
