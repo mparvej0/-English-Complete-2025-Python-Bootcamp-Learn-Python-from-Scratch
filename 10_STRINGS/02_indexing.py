896946b8e858c59068cc4d8bf7fbe66b556312ff
MD PARVEJ <mparvejind@gmail.com>
2025-08-17 07:58:52 +0530

    .

---
+++ 10_STRINGS/02_indexing.py
@@ -0,0 +1,14 @@
+name = "Raju"
+
+# name = "R  a  j  u"
+#         0  1  2  3
+#        -4 -3 -2 -1
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+
+print(name[-1])
+print(name[-2])
+print(name[-3]) # name[-3+4] name[1]
+print(name[-4])
