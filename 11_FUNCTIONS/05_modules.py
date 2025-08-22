53e988a549132491327f3f8f68e4c62055c411a2
MD PARVEJ <mparvejind@gmail.com>
2025-08-22 17:39:21 +0530

    .

--- 11_FUNCTIONS/05_modules.py
+++ 11_FUNCTIONS/05_modules.py
@@ -4,5 +4,10 @@
 #  List of all the built in Modules: https://docs.pythojn.org/3/
 import math
 import os
+import mymodule
+import requests
 
 print(math.sqrt(16))
+mymodule.hello()
+r = requests.get("https://www.google.com")
+print(r.text)
