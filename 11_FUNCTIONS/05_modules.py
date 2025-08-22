a0882b3a62a075462ed027e3cf25b9bb1d6eac70
MD PARVEJ <mparvejind@gmail.com>
2025-08-22 17:28:01 +0530

    .

---
+++ 11_FUNCTIONS/05_modules.py
@@ -0,0 +1,8 @@
+# Two types of modules in Python:
+#  - Build in Modules
+#  - External Modules
+#  List of all the built in Modules: https://docs.pythojn.org/3/
+import math
+import os
+
+print(math.sqrt(16))
