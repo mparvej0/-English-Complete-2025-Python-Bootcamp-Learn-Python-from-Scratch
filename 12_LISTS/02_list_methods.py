a5a58aad59c6b927bbcb7595f27d6d654609d201
MD PARVEJ <mparvejind@gmail.com>
2025-10-30 07:07:59 +0530

    .

---
+++ 12_LISTS/02_list_methods.py
@@ -0,0 +1,7 @@
+marks = [5, 2, 2, 5, 7]
+
+print((marks))
+# marks.append((63)) # This will change the original list
+# marks.pop()
+marks
+print(marks)
---
+++ PRACTICE_SET/questions1.py
@@ -0,0 +1,9 @@
+print("Hello World! Welcome to Python")
+
+print("Twinkle, twinkle star,/nHow I wonder waht you are!")
+
+name = "Shivani"
+age = 19
+height = 160.4
+is_student = True
+print("The name of the student is " + name + ". she " + age + " years old. " + "She is ")
