dab5664aa6e6137eef3f05ec7a3ebb82344d097f
MD PARVEJ <mparvejind@gmail.com>
2025-10-30 07:35:05 +0530

    .

--- PRACTICE_SET/questions1.py
+++ PRACTICE_SET/questions1.py
@@ -6,4 +6,24 @@ name = "Shivani"
 age = 19
 height = 160.4
 is_student = True
-print("The name of the student is " + name + ". she " + age + " years old. " + "She is ")
+print("The name of the student is " + name + ". she " + str(age) + " years old. " + "She is " + str(height) + " cms tall. " + "\nStudent: " + str(is_student))
+
+num = "45"
+print(int(num) + 10)
+
+food = input("what is your favorite fod?\n")
+print("Wow I also like " + food)
+
+a = int(input("Enter first number\n"))
+b = int(input("Enter second number\n"))
+
+print("a + b is ", a + b)
+print("a - b is ", a - b)
+print("a * b is ", a * b)
+print("a / b is ", a / b)
+
+print('Raju said, "Python is awesome!"\nThis is on a new line.\nThis is a tab ->\t<- here')
+
+a = int(input("Enter your number \n"))
+print("Square is ", a**2)
+print("Cube is ", a**3)
